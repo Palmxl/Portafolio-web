0ee94386b1114fcc392cda69268be6398dfcd1de
Palmxl <jmartin_sanchez@javeriana.edu.co>
2025-06-13 22:35:02 -0500

    Config tailwind, Index comenzado

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,25 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ["./index.html", "./scripts/**/*.js", "./pages/**/*.html", "./components/**/*.html"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundSize: {
+        'size-200': '200% 200%',
+      },
+      backgroundPosition: {
+        'pos-0': '0% 0%',
+        'pos-100': '100% 100%',
+      },
+      animation: {
+        gradient: 'gradient 8s ease infinite',
+      },
+      keyframes: {
+        gradient: {
+          '0%, 100%': { 'background-position': '0% 0%' },
+          '50%': { 'background-position': '100% 100%' },
+        }
+      }
+    },
   },
   plugins: [],
 }
